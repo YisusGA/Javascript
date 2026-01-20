677a6f9235739f9d911374cda85f6afa61857eff
Yisus <jga547@educa.madrid.org>
2026-01-20 17:29:58 +0100

    completada T25

--- mi_workspace/Tareas/T25/T25Objetos.js
+++ mi_workspace/Tareas/T25/T25Objetos.js
@@ -25,3 +25,24 @@ console.log("Aula: " + clase.aula + ". Número de alumnos: " + clase.numAlumnos)
 clase.turno = "mañana";
 clase.cursoActivo = true;
 console.log(clase);
+
+//Parte 4. Trabajo con métodos
+console.log(clase.mostrarInfo());
+//La palabra this dentro del método mostrarInfo() cumple la función esencial de indicarle al método que nos estamos refiriendo a la propiedad definida dentro del objeto, y no a cualquier otra propiedad con el mismo nombre que se encuentre fuera del objeto
+
+//Parte 5. Modificación dinámica del objeto
+clase.aula = "Aula 204";
+clase.numAlumnos = 25;
+clase.resumen = function() {
+  return `${this.curso} - ${this.aula} - ${this.numAlumnos} alumnos`;
+};
+console.log("Probando el nuevo método: ", clase.resumen());
+
+//Parte 6. Eliminación de propiedades
+delete clase.cursoActivo;
+console.log(clase);
+
+//Parte 7. Objetos y referencia
+const otraClase = clase;
+otraClase.aula = "Aula 300";
+console.log("Nueva aula en el objeto otraClase: ", otraClase.aula);
