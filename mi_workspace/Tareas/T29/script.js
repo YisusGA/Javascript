b59e1d68b90f6f94420815f0bc0f426e46cf50e8
Yisus <jga547@educa.madrid.org>
2026-02-03 19:37:46 +0100

    continuada T29

--- mi_workspace/Tareas/T29/script.js
+++ mi_workspace/Tareas/T29/script.js
@@ -1,11 +1,24 @@
 const boton1 = document.getElementById("btn1");
 const boton2 = document.getElementById("btn2");
 const boton3 = document.getElementById("btn3");
+const reset1 = document.getElementById("reset");
 const caja = document.getElementById("caja");
 
-function cuadroRojo() {
+function reset() {
+    caja.className = ''; //Con esto de aquí, eliminamos cualquier nombre de clase que tenga asociado el objeto
+    caja.classList.add("caja");
+}
+
+function cajaRoja() {
     caja.classList.remove("caja");
     caja.classList.add("cajaRoja");
 }
 
-boton1.addEventListener("click", cuadroRojo);
+function cajaRojaChiquita() {
+    caja.classList.remove("caja");
+    caja.classList.add("cajaRojaChiquita");
+}
+
+boton1.addEventListener("click", cajaRoja);
+boton1.addEventListener("dblclick", cajaRojaChiquita);
+reset1.addEventListener("click", reset);
