467c965c76afbaba8079a87fff3eb25398d77faf
Yisus <jga547@educa.madrid.org>
2026-02-06 20:36:44 +0100

    hecha T31 de DOM y formularios

---
+++ mi_workspace/Tareas/T31/script.js
@@ -0,0 +1,92 @@
+
+//VARIABLES
+
+//Formulario
+const form = document.getElementById("formulario");
+
+//Campos de nombre y correo
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("email");
+
+//Checkbox de condiciones
+const condiciones = document.getElementById("condiciones");
+
+//Lista de botones radio
+const tipoUsuario = document.getElementsByName("tipo");
+const checkboxProfesor = document.getElementById("profesor");
+const checkboxAlumno = document.getElementById("alumno");
+
+//Mensaje a mostrar
+const mensaje = document.getElementById("resultado");
+
+//Botón de reset
+const reset = document.getElementById("reset");
+
+//FUNCIONES
+
+//Función de campo activo para el campo nombre
+function campoActivoNombre() {
+    nombre.classList.add("campoActivo");
+}
+
+//Función de campo inactivo para el campo nombre
+function campoInactivoNombre() {
+    nombre.className = "";
+}
+
+//Función de campo activo para el campo email
+function campoActivoEmail() {
+    correo.classList.add("campoActivo");
+}
+
+//Función de campo inactivo para el campo nombre
+function campoInactivoEmail() {
+    correo.className = "";
+}
+
+//Función para resetear el contenido del mensaje y su clase asociada
+function resetStyle () {
+    mensaje.textContent = "";
+    mensaje.className = "";
+}
+
+//Función para validar que el formulario se ha rellenado correctamente
+function validarFormulario(event) {
+    event.preventDefault();
+    if (nombre.value === "" || email.value === "" || !condiciones.checked || (!checkboxAlumno.checked && !checkboxProfesor.checked)) {
+        mensaje.textContent = "Todos los campos son obligatorios";
+        mensaje.classList.add("error");
+    } else {
+        let radioSelected = ""; //Importante que sea let, porque tenemos que reasignar la variable dentro del if
+        for (let i = 0; i < tipoUsuario.length; i++) {
+            if (tipoUsuario[i].checked) {
+                radioSelected = tipoUsuario[i].value;
+            }
+        }
+        mensaje.classList.add("correcto");
+        //Esta es una forma de modificar el texto de un elemento pero usando formato, con innerHTML, usando `` y las variables contenidas con ${}. Se pueden usar etiquetas de HTML
+        mensaje.innerHTML = `
+        <p><strong>Datos introducidos correctamente</strong></p>
+        <p><strong>Nombre:</strong> ${nombre.value}</p>
+        <p><strong>Email:</strong> ${email.value}</p>
+        <p><strong>Acepta condiciones:</strong> Sí</p>
+        <p><strong>Tipo de usuario:</strong> ${radioSelected}</p>
+        `;
+    }
+}
+
+//EVENTOS
+
+//Campo activo para nombre y email
+nombre.addEventListener("focus", campoActivoNombre);
+correo.addEventListener("focus", campoActivoEmail);
+
+//Campo inactivo para nombre y email
+nombre.addEventListener("blur", campoInactivoNombre);
+correo.addEventListener("blur", campoInactivoEmail);
+
+//Reset
+reset.addEventListener("click", resetStyle);
+
+//Validar formulario
+form.addEventListener("submit", validarFormulario);
